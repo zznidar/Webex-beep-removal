676f727bf64552dc69a568b9df0a188fd70526e2
zznidar <zznidar@users.noreply.github.com>
2020-10-10 18:44:14 +0200

    Add code, usage

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Webex-beep-removal
 Mein erster und bester Versuch of creating a filter to filter annoying beeping whenever someone joins or leaves a Cisco Webex meeting on-the-fly!
+
+## Usage
+1. Join a meeting from your browser (tested in Firefox and Chrome)
+2. Open _DevTools_ (`F12` on your keyboard)
+3. Switch to the _Console_ tab of DevTools
+4. Copy code from https://raw.githubusercontent.com/zznidar/Webex-beep-removal/main/code.js
+5. Paste it into the Console, apply with `Enter`
+6. Migrainelessly enjoy the meeting with beeps greatly reduced!
---
+++ code.js
@@ -0,0 +1,30 @@
+frekvence = [589, 1293, 453, 1050, 455, 744];
+kju = 2;
+tip = "notch";
+gaining = 2;
+
+context = new AudioContext();
+source = context.createMediaElementSource(document.getElementsByTagName('video')[0]);
+
+lastFilter = source;
+for(f of frekvence) {
+	let filter = context.createBiquadFilter();
+		
+	filter.type = tip;
+	filter.frequency.value = f;
+	filter.Q.value = kju;
+	
+	lastFilter.connect(filter);
+	
+	lastFilter = filter;
+}
+
+// gainamo, da bo dovolj glasno
+var gainNode = context.createGain();
+gainNode.gain.value = gaining;
+
+lastFilter.connect(gainNode);
+
+lastFilter = gainNode;
+
+lastFilter.connect(context.destination);
